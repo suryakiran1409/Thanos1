cd2c9268886da99272d0ab49a7006833d3ab9396
suryakiran1409 <suryakiran1409@yahoo.com>
2018-05-22 22:32:07 +0530

    updated code

--- app/src/main/java/com/example/prachu/thanos/MainActivity.java
+++ app/src/main/java/com/example/prachu/thanos/MainActivity.java
@@ -38,66 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
      rl=(RelativeLayout)findViewById(R.id.relativeLayout);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    for(int j=1;j>0;j++)
-                    {
-                        Random r= new Random();
-                        n= r.nextInt(6);
-                        if(a[0]==1 && a[1]==1 && a[2]==1 && a[3]==1 && a[4]==1 && a[5]==1 ) {
-                            tv1.setText("! hurray you have collected all stones !");
-                            break;
-                        }
-                        if(a[n]==0)
-                        {
-                           switch(n)
-                           {
-                               case 0: { obb="Power stone ";
-                                   rl.setBackgroundColor(Color.parseColor("#1D3AC9"));
-                               break; }
-                               case 1: { obb="space stone ";
-                               rl.setBackgroundColor(Color.parseColor("#7EE1F5"));
-                               break; }
-                               case 2: { obb="Time stone ";
-                               rl.setBackgroundColor(Color.parseColor("#34F61D"));
-                               break; }
-                               case 3: { obb="Reality stone ";
-                               rl.setBackgroundColor(Color.parseColor("#E62630"));
-                               break;  }
-                               case 4:{  obb="soul stone ";
-                               rl.setBackgroundColor(Color.parseColor("#FB4925"));
-                               break; }
-                               case 5: { obb="Mind stone";
-                               rl.setBackgroundColor(Color.parseColor("#F8ED23"));
-                               break;  }
-                           }
-                           a[n]=1;
-                            if(a[0]==1 && a[1]==1 && a[2]==1 && a[3]==1 && a[4]==1 && a[5]==1 ) {
-                                tv1.setText("! hurray you have collected all stones !");
-                                stones= stones+"\n" + obb;
-                                break;
-                            }
-                           stones= stones+"\n" + obb;
-                           break;
-                        }
-                    }
-                    tv2.setText(" "+ stones);
-                    tv3.setText("You Have got the "+ obb);
-            }
-        });
-
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv1.setText(" ");
-                tv2.setText(" list of stones obtained : ");
-                tv3.setText("stone obtained now is : ");
-                rl.setBackgroundColor(Color.parseColor("#f8e0a9"));
-                a[0]=0;a[1]=0;a[2]=0;a[3]=0;a[4]=0;a[5]=0;
-                stones="List of stones obtained : ";
-            }
-        });
     }
 
 
@@ -132,5 +72,63 @@ public class MainActivity extends AppCompatActivity {
         a[4]= savedInstanceState.getInt("array4");
         a[5]= savedInstanceState.getInt("array5");
 
+    }
+
+    public void infinity(View v)
+    {
+        for(int j=1;j>0;j++)
+        {
+            Random r= new Random();
+            n= r.nextInt(6);
+            if(a[0]==1 && a[1]==1 && a[2]==1 && a[3]==1 && a[4]==1 && a[5]==1 ) {
+                tv1.setText("! hurray you have collected all stones !");
+                break;
+            }
+            if(a[n]==0)
+            {
+                switch(n)
+                {
+                    case 0: { obb="Power stone ";
+                        rl.setBackgroundColor(Color.parseColor("#1D3AC9"));
+                        break; }
+                    case 1: { obb="space stone ";
+                        rl.setBackgroundColor(Color.parseColor("#7EE1F5"));
+                        break; }
+                    case 2: { obb="Time stone ";
+                        rl.setBackgroundColor(Color.parseColor("#34F61D"));
+                        break; }
+                    case 3: { obb="Reality stone ";
+                        rl.setBackgroundColor(Color.parseColor("#E62630"));
+                        break;  }
+                    case 4:{  obb="soul stone ";
+                        rl.setBackgroundColor(Color.parseColor("#FB4925"));
+                        break; }
+                    case 5: { obb="Mind stone";
+                        rl.setBackgroundColor(Color.parseColor("#F8ED23"));
+                        break;  }
+                }
+                a[n]=1;
+                if(a[0]==1 && a[1]==1 && a[2]==1 && a[3]==1 && a[4]==1 && a[5]==1 ) {
+                    tv1.setText("! hurray you have collected all stones !");
+                    stones= stones+"\n" + obb;
+                    break;
+                }
+                stones= stones+"\n" + obb;
+                break;
+            }
+        }
+        tv2.setText(" "+ stones);
+        tv3.setText("You Have got the "+ obb);
+    }
+
+    public void reset(View v)
+    {
+
+        tv1.setText(" ");
+        tv2.setText(" list of stones obtained : ");
+        tv3.setText("stone obtained now is : ");
+        rl.setBackgroundColor(Color.parseColor("#f8e0a9"));
+        a[0]=0;a[1]=0;a[2]=0;a[3]=0;a[4]=0;a[5]=0;
+        stones="List of stones obtained : ";
     }
 }
